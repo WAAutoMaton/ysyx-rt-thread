4ed46aca44149be635c2961d4353d628e22c70ce
Zihao Yu <yuzihao@ict.ac.cn>
2023-05-24 15:14:44 +0800

    bsp,abstract-machine,libc: add more dummy functions to pass linking

--- bsp/abstract-machine/src/libc.c
+++ bsp/abstract-machine/src/libc.c
@@ -5,6 +5,18 @@ char *strchr(const char *s, int c) {
   assert(0);
 }
 
+char *strrchr(const char *s, int c) {
+  assert(0);
+}
+
 char *strstr(const char *haystack, const char *needle) {
   return rt_strstr(haystack, needle);
+}
+
+long strtol(const char *restrict nptr, char **restrict endptr, int base) {
+  assert(0);
+}
+
+char *strncat(char *restrict dst, const char *restrict src, size_t sz) {
+  assert(0);
 }
