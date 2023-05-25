475f866a45527f014c0c93040125dd559314a303
Zihao Yu <yuzihao@ict.ac.cn>
2023-05-26 00:13:38 +0800

    bsp,abstract-machine,uart: add more preset commands

--- bsp/abstract-machine/src/uart.c
+++ bsp/abstract-machine/src/uart.c
@@ -37,7 +37,7 @@ static int _uart_putc(struct rt_serial_device *serial, char c) {
 }
 
 static int _uart_getc(struct rt_serial_device *serial) {
-  static const char *p = "help\n";
+  static const char *p = "help\ndate\nversion\nfree\nps\npwd\nls\nmemtrace\nmemcheck\nutest_list\n";
   return (*p != '\0' ? *(p ++) : -1);
 }
 
